27afca8c8f72fdcb74d906d365251c7fecae67a6
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2024-02-07 05:05:48 +0000

    starting route one_20

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get("/",{controller:"home", action:"home"})
-  get("/dice/2/6",{controller:"home",action:"two_6"})
-  get("/dice/2/10",{controller:"home",action:"two_10"})
+  get("/dice/2/6", {controller:"home",action:"two_6"})
+  get("/dice/2/10", {controller:"home",action:"two_10"})
+  get("/dice/1/20", {controller:"home", action:"one_20"})
 end
